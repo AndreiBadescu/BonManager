cdc4bf2cb39bc0fb339f1add14a896735b18bbf1
Andrei <andreibadescu18@yahoo.com>
2021-12-05 16:31:59 +0200

    Added "select image button"

--- app/src/main/java/com/bonmanager/MainActivity.java
+++ app/src/main/java/com/bonmanager/MainActivity.java
@@ -1,6 +1,16 @@
 package com.bonmanager;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,9 +22,19 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.bonmanager.databinding.ActivityMainBinding;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ImageButton addImageButton;
+    private ImageButton selectImageFromGalleyButton;
+    private ImageView receiptImage;
+    private Bitmap imageBitmap;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PICK_IMAGE = 5;
+    private static int SELECT_PICTURE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +52,87 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        receiptImage = (ImageView) findViewById(R.id.image);
+
+        addImageButton = (ImageButton) findViewById(R.id.add_image_btn);
+        addImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        selectImageFromGalleyButton = (ImageButton) findViewById(R.id.select_image_from_gallery);
+        selectImageFromGalleyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchSelectImageFromGalleryIntent();
+            }
+        });
     }
 
+    private void dispatchTakePictureIntent() {
+        // in the method we are displaying an intent to capture our image.
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // on below line we are calling a start activity
+        // for result method to get the image captured.
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private void dispatchSelectImageFromGalleryIntent() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    private String getRealPathFromURIForGallery(Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = this.getContentResolver().query(uri, projection, null,
+                null, null);
+        if (cursor != null) {
+            int column_index =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        assert false;
+        cursor.close();
+        return uri.getPath();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK) { return; }
+        // calling on activity result method.
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            // on below line we are getting
+            // data from our bundles. .
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            // below line is to set the
+            // image bitmap to our image.
+            receiptImage.setImageBitmap(imageBitmap);
+        } else if (requestCode == PICK_IMAGE || requestCode == SELECT_PICTURE) {
+            Uri selectedImageUri = data.getData();
+            if (selectedImageUri != null) {
+                String selectedImagePath = getRealPathFromURIForGallery(selectedImageUri);
+                System.out.println(selectedImagePath);
+                imageBitmap = (Bitmap) BitmapFactory.decodeFile(selectedImagePath);
+                receiptImage.setImageBitmap(imageBitmap);
+            }
+        }
+    }
 }
